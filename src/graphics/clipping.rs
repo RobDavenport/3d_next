dd41274336961c442b8c63cc344930d5d9017b16
RobDavenport <zerve0@hotmail.com>
2024-02-22 15:32:19 +0900

    fix clipping

--- src/graphics/clipping.rs
+++ src/graphics/clipping.rs
@@ -1,4 +1,3 @@
-use gamercade_rs::api::text::console_log;
 use glam::Vec4;
 
 use crate::{camera::NEAR_PLANE, shaders::PixelShaderInput};
@@ -43,11 +42,6 @@ fn clip_triangle_one_front<P: PixelShaderInput>(
     triangle: Triangle<P>,
     front_index: usize,
 ) -> Triangle<P> {
-    console_log("clip: one in front");
-    console_log(&format!("front: {}", triangle.positions[front_index]));
-    console_log(&format!(" t 1 : {}", triangle.positions[(front_index + 1) % 3]));
-    console_log(&format!(" t 2 : {}", triangle.positions[(front_index + 2) % 3]));
-
     clip_edges_against_near_plane(triangle, front_index)
 }
 
@@ -74,8 +68,8 @@ fn clip_edges_against_near_plane<P: PixelShaderInput>(
     let aw_clipped = a.w - NEAR_PLANE;
 
     // Vectors from A->B and A->C
-    let ab = b.w - aw_clipped;
-    let ac = c.w - aw_clipped;
+    let ab = (b.w - NEAR_PLANE) - aw_clipped;
+    let ac = (c.w - NEAR_PLANE) - aw_clipped;
 
     // Find how much to lerp
     // Negate aw_clipped as we use reversed depth
@@ -101,11 +95,6 @@ fn clip_triangle_one_behind<P: PixelShaderInput>(
     triangle: Triangle<P>,
     back_index: usize,
 ) -> (Triangle<P>, Triangle<P>) {
-    console_log("clip: one behind");
-    console_log(&format!("back: {}", triangle.positions[back_index]));
-    console_log(&format!(" t 1 : {}", triangle.positions[(back_index + 1) % 3]));
-    console_log(&format!(" t 2 : {}", triangle.positions[(back_index + 2) % 3]));
-
     // Index Setup - o0 isn't used as it's the vertex being split
     let o1 = (back_index + 1) % 3;
     let o2 = (back_index + 2) % 3;
