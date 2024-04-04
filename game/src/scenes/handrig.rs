d9a272aa5920f3ccc480e3bf4ede26028029236c
RobDavenport <zerve0@hotmail.com>
2024-04-05 01:06:12 +0900

    add mech test

--- game/src/scenes/handrig.rs
+++ game/src/scenes/handrig.rs
@@ -3,7 +3,7 @@ use glam::Mat4;
 use crate::{
     actor::Actor,
     animation::Animator,
-    generated::meshes,
+    generated::{meshes, textures},
     shaders::{Animated, TexturedLit},
 };
 
@@ -11,22 +11,22 @@ use super::Scene;
 
 pub struct HandScene {
     mesh: Actor<5>,
-    shader: Animated<10, 4>,
+    shader: Animated<19, 4>,
 }
 
 impl HandScene {
     pub fn new() -> Self {
         let shader = Animated {
             animator: Animator::new(
-                meshes::HANDRIG_SKL.as_skeleton(),
-                meshes::HANDRIG_SKN.as_skin(),
-                meshes::HANDRIG_THUMBSUP_ANM.as_anim(),
+                meshes::MECH_SKL.as_skeleton(),
+                meshes::MECH_SKN.as_skin(),
+                meshes::MECH_IDLE_ANM.as_anim(),
             ),
         };
 
         Self {
             mesh: Actor {
-                mesh: meshes::HANDRIG.as_mesh(),
+                mesh: meshes::MECH.as_mesh(),
                 transform: Mat4::IDENTITY,
                 delta: 0.0,
             },
@@ -38,7 +38,7 @@ impl HandScene {
 impl Scene for HandScene {
     fn draw(&self, gpu: &mut crate::graphics::Gpu) {
         gpu.uniforms.model = self.mesh.transform;
-        gpu.uniforms.diffuse = meshes::HANDRIG_0_TEX.as_texture();
+        gpu.uniforms.diffuse = textures::ENDESGA32_TEX.as_texture();
         gpu.render_mesh(self.mesh.mesh, self.shader, TexturedLit);
     }
 
